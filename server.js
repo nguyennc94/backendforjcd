69ecfc78a7a6ebd9bdd5e95bc0971ee8fcd1238c
Nguyen <Nnguyen52@aceproject.space>
2021-08-25 20:35:02 -0500

    test9

--- server.js
+++ server.js
@@ -1,102 +1,90 @@
 const express = require('express');
 const cors = require('cors');
-const PORT = process.env.PORT || 5000;
+const PORT = 5001;
 const app = express();
 const mongoose = require('mongoose')
-const { MongoClient } = require('mongodb');
+const {MongoClient} = require('mongodb');
 const axios = require("axios");
-const cool = require('cool-ascii-faces');
 
 app.use(cors());
 // const corsOptions = {
 //     origin: "http://localhost:3000"
 // };
 
-app.get('/', async (req, res) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  let body = `<?xml version="1.0"?> 
+// app.get('/', async (req, res) => {
+//     res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+//     res.header(
+//       "Access-Control-Allow-Headers",
+//       "Origin, X-Requested-With, Content-Type, Accept"
+//     );
+//     let body = `<?xml version="1.0"?> 
 
-    <transaction xmlns="http://www.metrex.net/momex/transaction#" 
+//     <transaction xmlns="http://www.metrex.net/momex/transaction#" 
     
-                 xmlns:momex="http://www.metrex.net/momex#" 
+//                  xmlns:momex="http://www.metrex.net/momex#" 
     
-                 xmlns:pw="http://www.pharmacywire.com/" 
+//                  xmlns:pw="http://www.pharmacywire.com/" 
     
-                 type="Catalog" 
+//                  type="Catalog" 
     
-                 local="true"> 
+//                  local="true"> 
     
-                 <momex:authenticate momex:username="xmlconnect_25" 
+//                  <momex:authenticate momex:username="xmlconnect_25" 
     
-                 momex:password="984@qSv@rps@R9F"/> 
+//                  momex:password="984@qSv@rps@R9F"/> 
     
      
     
-    </transaction> 
-  `;
-
-  const response = await axios.post("https://jpp.test.pharmacywire.com/momex/NavCode/xmlconnect", body);
-  const a = response.data
-  const jsonResponse = JSON.stringify(a);
-  res.send(jsonResponse);
-
-
-  async function main() {
-    /**
-     * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
-     * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
-     */
-    const uri = `${process.env.MONGODB_URI}`;
+//     </transaction> 
+//   `;
+  
+//         const response = await axios.post("https://jpp.test.pharmacywire.com/momex/NavCode/xmlconnect",body);
+//         const a = response.data
+// //       const jsonResponse = JSON.stringify(arr);    
+// //       res.send(jsonResponse);
 
 
-    // const client = new MongoClient(uri);
-    mongoose.connect(process.env.MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    })
+//       async function main(){
+//         /**
+//          * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
+//          * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
+//          */
+//         const uri = "mongodb+srv://canadadrugstore:02061994@cluster0.issgz.mongodb.net/PW?retryWrites=true&w=majority";
+      
+      
+//         const client = new MongoClient(uri);
+      
+//         try {
+//             // Connect to the MongoDB cluster
+//             await client.connect();
+      
+//             // Make the appropriate DB calls
+//             await createListing(client,{
+//               name: "PWdata",
+//               data: a
+//             })
+      
+//         } catch (e) {
+//             console.error(e);
+//         } finally {
+//             await client.close();
+//         }
+//       }
+      
+//       main().catch(console.error);
+      
+//       async function createListing( client, newListing){
+//         const result = await client.db("PW").collection("PWtest")
+//         .insertOne(newListing)
+      
+//         console.log(`New listing create with id: ${result.insertedId}`)
+//       }
+//   });
 
-    mongoose.connection.on('connected', () => {
-      createListing(client, {
-        name: "PWdata",
-        data: a
-      })
-    })
-
-    // try {
-    // Connect to the MongoDB cluster
-    // await client.connect();
-
-    // Make the appropriate DB calls
-    // await createListing(client, {
-    // name: "PWdata",
-    // data: a
-    //     })
-
-    //   } catch (e) {
-    //     console.error(e);
-    //   } finally {
-    //     await client.close();
-    //   }
-    // }
-
-    // main().catch(console.error);
-  }
-  async function createListing(client, newListing) {
-    const result = await client.db("PW").collection("PWtest")
-      .insertOne(newListing)
-
-    console.log(`New listing create with id: ${result.insertedId}`)
-  }
-});
-app.get('/cool', (req, res) => res.send(cool()))
 //api endpoints
 app.use('/login', require('./routes/loginserver'));
 app.use('/catalog', require('./routes/catalogserver'));
 
 app.listen(PORT, () => {
-  console.log(`Example app listening at http://localhost:${PORT}`);
+    console.log(`Example app listening at http://localhost:${PORT}`);
 });
