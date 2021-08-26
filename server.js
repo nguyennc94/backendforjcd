2b9cfa5af05a5d18b5c751d5798f3949c5aafda5
Nguyen <Nnguyen52@aceproject.space>
2021-08-25 21:59:55 -0500

    test10

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const PORT = 5001;
 const app = express();
 const mongoose = require('mongoose')
-const {MongoClient} = require('mongodb');
+const { MongoClient } = require('mongodb');
 const axios = require("axios");
 
 app.use(cors());
@@ -20,24 +20,24 @@ app.use(cors());
 //     let body = `<?xml version="1.0"?> 
 
 //     <transaction xmlns="http://www.metrex.net/momex/transaction#" 
-    
+
 //                  xmlns:momex="http://www.metrex.net/momex#" 
-    
+
 //                  xmlns:pw="http://www.pharmacywire.com/" 
-    
+
 //                  type="Catalog" 
-    
+
 //                  local="true"> 
-    
+
 //                  <momex:authenticate momex:username="xmlconnect_25" 
-    
+
 //                  momex:password="984@qSv@rps@R9F"/> 
-    
-     
-    
+
+
+
 //     </transaction> 
 //   `;
-  
+
 //         const response = await axios.post("https://jpp.test.pharmacywire.com/momex/NavCode/xmlconnect",body);
 //         const a = response.data
 // //       const jsonResponse = JSON.stringify(arr);    
@@ -50,40 +50,86 @@ app.use(cors());
 //          * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
 //          */
 //         const uri = "mongodb+srv://canadadrugstore:02061994@cluster0.issgz.mongodb.net/PW?retryWrites=true&w=majority";
-      
-      
+
+
 //         const client = new MongoClient(uri);
-      
+
 //         try {
 //             // Connect to the MongoDB cluster
 //             await client.connect();
-      
+
 //             // Make the appropriate DB calls
 //             await createListing(client,{
 //               name: "PWdata",
 //               data: a
 //             })
-      
+
 //         } catch (e) {
 //             console.error(e);
 //         } finally {
 //             await client.close();
 //         }
 //       }
-      
+
 //       main().catch(console.error);
-      
+
 //       async function createListing( client, newListing){
 //         const result = await client.db("PW").collection("PWtest")
 //         .insertOne(newListing)
-      
+
 //         console.log(`New listing create with id: ${result.insertedId}`)
 //       }
 //   });
 
+
+app.get('catalog/drug/:drug', async (req, res) => {
+    res.setHeader("Access-Control-Allow-Origin", "http://localhost:3001");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    );
+
+    let body = `<?xml version="1.0"?> 
+  
+    <transaction xmlns="http://www.metrex.net/momex/transaction#" 
+    
+                 xmlns:momex="http://www.metrex.net/momex#" 
+    
+                 xmlns:pw="http://www.pharmacywire.com/" 
+    
+                 type="Catalog" 
+    
+                 local="true"> 
+    
+        <momex:authenticate momex:username="xmlconnect_25" 
+      
+                                momex:password="984@qSv@rps@R9F"/> 
+    
+        <momex:criteria> 
+    
+            <pw:drug-package-option pw:include-zero-priced="true" 
+    
+                   pw:include-inactive="false"/> 
+    
+            <pw:drug-pics-search>${req.params.drug}</pw:drug-pics-search> 
+    
+     
+        </momex:criteria> 
+    
+    </transaction> 
+  `;
+
+    const response = await axios.post("https://jpp.test.pharmacywire.com/momex/NavCode/xmlconnect", body);
+    const a = response.data
+    console.log(response)
+    const jsonResponse = JSON.stringify(a);
+    res.send(jsonResponse);
+
+})
+
 //api endpoints
 app.use('/login', require('./routes/loginserver'));
-app.use('/catalog', require('./routes/catalogserver'));
+// app.use('/catalog', require('./routes/catalogserver'));
 
 app.listen(PORT, () => {
     console.log(`Example app listening at http://localhost:${PORT}`);
