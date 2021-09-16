2698b6b58751c92c0dcd5ce50196dd32307c9e3e
Nguyen Nguyen <nnguyen52@Academic.RRC.ca>
2021-09-16 17:36:14 -0500

    Cibbectui

--- routes/submitorderserver.js
+++ routes/submitorderserver.js
@@ -9,34 +9,22 @@ router.get(`/id/:id/items/:items/coupon/:coupon/addressscript/:addressscript/del
       "Origin, X-Requested-With, Content-Type, Accept"
     );
     let body = `<?xml version="1.0"?>
-
     <transaction	xmlns="http://www.metrex.net/momex/transaction#"
+              xmlns:momex="http://www.metrex.net/momex#"
+              xmlns:pw="http://www.pharmacywire.com/"
+              type="SubmitOrder"
+              local="true">
+          <momex:authenticate momex:username="xmlconnect_25" momex:password="984@qSv@rps@R9F" />
+    <shippingfee>$25</shippingfee>
+      <pw:patient momex:id="${req.params.id}"/>
+      <order>
+        <items>
+        ${req.params.items}
+        </items>
+        ${req.params.addressscript}
+        ${req.params.payment}
+      </order>
 
-            xmlns:momex="http://www.metrex.net/momex#"
-
-            xmlns:pw="http://www.pharmacywire.com/"
-
-            type="SubmitOrder"
-
-            local="true">
-
-    <momex:authenticate momex:username="xmlconnect_25" momex:password="984@qSv@rps@R9F" />
-
-    <pw:patient momex:id="${req.params.id}"/>
-
-    <order>
-    <items>
-    ${req.params.items}
-    <item momex:coupon-code="${req.params.coupon}"></item>
-
-    </items>
-
-    ${req.params.addressscript}
-    <shippingfee>${req.params.deliverymethod}</shippingfee>
-
-   ${req.params.payment}
-
-    </order>
 
     </transaction>
            `
