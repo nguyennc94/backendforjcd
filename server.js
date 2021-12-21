ae335f707746eb64950e6e2973e6f0f92902a895
Nguyen <Nnguyen52@aceproject.space>
2021-12-21 10:09:44 -0600

    Change creating database in order server

--- server.js
+++ server.js
@@ -17,68 +17,7 @@ app.get('/', async (req, res) => {
       "Access-Control-Allow-Headers",
       "Origin, X-Requested-With, Content-Type, Accept"
     );
-    let body = `<?xml version="1.0"?>
 
-    <transaction xmlns="http://www.metrex.net/momex/transaction#"
-
-                 xmlns:momex="http://www.metrex.net/momex#"
-
-                 xmlns:pw="http://www.pharmacywire.com/"
-
-                 type="Catalog"
-
-                 local="true">
-
-                 <momex:authenticate momex:username="xmlconnect_9"
-
-                 momex:password="EZZ!C7F!68Y!9w3"/>
-
-
-
-    </transaction>
-  `;
-
-        const response = await axios.post("https://jpp.pharmacywire.com/momex/NavCode/xmlconnect",body);
-        const a = response.data
-      const jsonResponse = JSON.stringify(a);
-      res.send(jsonResponse);
-
-
-      async function main(){
-        /**
-         * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
-         * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
-         */
-        const uri = "mongodb+srv://canadadrugstore:Health4Ever!@cluster0.1zdtx.mongodb.net/Catalog?retryWrites=true&w=majority";
-
-
-        const client = new MongoClient(uri);
-
-        try {
-            // Connect to the MongoDB cluster
-            await client.connect();
-
-            // Make the appropriate DB calls
-            await createListing(client,{
-              name: "PWdata",
-              data: a
-            })
-
-        } catch (e) {
-            console.error(e);
-        } finally {
-            await client.close();
-        }
-      }
-
-      main().catch(console.error);
-
-      async function createListing( client, newListing){
-        const result = await client.db("Catalog").collection("AllProducts")
-        .insertOne(newListing)
-
-        console.log(`New listing create with id: ${result.insertedId}`)
-      }
   });
 
 //api endpoints
@@ -104,6 +43,7 @@ app.use('/uploadcamera', require('./routes/uploadcameraserver') )
 app.use('/userstatus', require('./routes/userstatusserver') )
 app.use('/resetpassword', require('./routes/resetpasswordserver') )
 app.use('/catalog1', require('./routes/catalogserver1'));
+app.use('/callcatalog', require('./routes/callcatalogserver'));
 
 
 app.listen(PORT, () => {
