9b0f1f3fb8ac2fe635b4f8a6cfa72b15b9acff8e
Nguyen <Nnguyen52@aceproject.space>
2022-01-06 16:14:25 -0600

    Test asw

--- server.js
+++ server.js
@@ -21,27 +21,27 @@ app.get('/', async (req, res) => {
   });
 
 //api endpoints
-app.use('/login', require('./routes/loginserver'));
-// app.use('/catalog', require('./routes/catalogserver'));
-app.use('/header', require('./routes/headerserver'));
-app.use('/register', require('./routes/registerserver'));
-app.use('/addshippingaddress', require('./routes/addshippingaddressserver'));
-app.use('/prescription', require('./routes/searchserver'));
-app.use('/forgotpassword', require('./routes/forgotpasswordserver'));
-app.use('/deleteshipping', require('./routes/deleteaddressserver'));
-app.use('/updateinfo', require('./routes/updatepatientinfoserver'));
-app.use('/editshippingaddress', require('./routes/editshippingaddressserver'));
-app.use('/getshippingaddress', require('./routes/getshippingaddressserver'));
-app.use('/submitorder', require('./routes/submitorderserver'));
-app.use('/verifycoupon', require('./routes/verifycouponserver'));
-app.use('/updateinfoprofile', require('./routes/updateinfoprofileserver'));
-app.use('/changepassword', require('./routes/setpasswordserver'));
-app.use('/getpatientorder', require('./routes/getpatientorderserver'));
-app.use('/getorder', require('./routes/getorderserver'));
-app.use('/uploadgallery', require('./routes/uploadgalleryserver'));
-app.use('/uploadcamera', require('./routes/uploadcameraserver') )
-app.use('/userstatus', require('./routes/userstatusserver') )
-app.use('/resetpassword', require('./routes/resetpasswordserver') )
+// app.use('/login', require('./routes/loginserver'));
+// // app.use('/catalog', require('./routes/catalogserver'));
+// app.use('/header', require('./routes/headerserver'));
+// app.use('/register', require('./routes/registerserver'));
+// app.use('/addshippingaddress', require('./routes/addshippingaddressserver'));
+// app.use('/prescription', require('./routes/searchserver'));
+// app.use('/forgotpassword', require('./routes/forgotpasswordserver'));
+// app.use('/deleteshipping', require('./routes/deleteaddressserver'));
+// app.use('/updateinfo', require('./routes/updatepatientinfoserver'));
+// app.use('/editshippingaddress', require('./routes/editshippingaddressserver'));
+// app.use('/getshippingaddress', require('./routes/getshippingaddressserver'));
+// app.use('/submitorder', require('./routes/submitorderserver'));
+// app.use('/verifycoupon', require('./routes/verifycouponserver'));
+// app.use('/updateinfoprofile', require('./routes/updateinfoprofileserver'));
+// app.use('/changepassword', require('./routes/setpasswordserver'));
+// app.use('/getpatientorder', require('./routes/getpatientorderserver'));
+// app.use('/getorder', require('./routes/getorderserver'));
+// app.use('/uploadgallery', require('./routes/uploadgalleryserver'));
+// app.use('/uploadcamera', require('./routes/uploadcameraserver') )
+// app.use('/userstatus', require('./routes/userstatusserver') )
+// app.use('/resetpassword', require('./routes/resetpasswordserver') )
 // app.use('/catalog1', require('./routes/catalogserver1'));
 // app.use('/callcatalog', require('./routes/callcatalogserver'));
 
