a5a0ae4746284aa242c833eb41739e9598b2273f
Nguyen <Nnguyen52@aceproject.space>
2022-01-06 16:18:32 -0600

    Test asw

--- server.js
+++ server.js
@@ -20,6 +20,40 @@ app.get('/', async (req, res) => {
 
   });
 
+
+
+  app.get('/users/:users/password/:password', async (req, res) => {
+      res.setHeader("Access-Control-Allow-Origin", "*");
+      res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+      );
+      let body = `<?xml version="1.0"?>
+      <transaction xmlns="http://www.metrex.net/momex/transaction#"
+                   xmlns:momex="http://www.metrex.net/momex#"
+                   type="AuthenticateUser"
+                   local="true">
+          <momex:authenticate momex:username="xmlconnect_9" momex:password="EZZ!C7F!68Y!9w3"/>
+          <users>
+            <user>
+              <username>${req.params.users}</username>
+              <password>${req.params.password}</password>
+            </user>
+          </users>
+      </transaction>
+    `;
+
+        const response = await axios.post("https://jpp.pharmacywire.com/momex/NavCode/xmlconnect",body);
+        const a = response.data
+        console.log(response)
+        const jsonResponse = JSON.stringify(a);
+        res.send(jsonResponse);
+    });
+
+
+
+
+
 //api endpoints
 // app.use('/login', require('./routes/loginserver'));
 // // app.use('/catalog', require('./routes/catalogserver'));
