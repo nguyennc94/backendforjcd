0a846149259a2d8b6212b87de485a431f30c1c5f
Nguyen <Nnguyen52@aceproject.space>
2022-02-09 12:56:09 -0600

    test

--- routes/getpatientpackageorderserver.js
+++ routes/getpatientpackageorderserver.js
@@ -9,17 +9,15 @@ router.get('/id/:id', async (req, res) => {
       "Origin, X-Requested-With, Content-Type, Accept"
     );
     let body = `<?xml version="1.0"?>
-    <transaction xmlns="http://www.metrex.net/momex/transaction#"
-    xmlns:momex="http://www.metrex.net/momex#"
-    xmlns:pw="http://www.pharmacywire.com/"
-    type="Catalog" local="true">
-    <momex:authenticate momex:username="xmlconnect_9"
-    momex:password="EZZ!C7F!68Y!9w3" />
-    <momex:criteria>
-    <pw:drug-package-option pw:include-zero-priced="false" pw:include-inactive="false"/>
-    <pw:package pw:id="${req.params.id}"/> <
-    /momex:criteria>
-    </transaction>
+          <transaction xmlns="http://www.metrex.net/momex/transaction#" xmlns:momex="http://www.metrex.net/momex#"
+          xmlns:pw="http://www.pharmacywire.com/"
+          type="Catalog" local="true">
+          <momex:authenticate momex:username="xmlconnect_9"
+          momex:password="EZZ!C7F!68Y!9w3" />
+          <momex:criteria>
+          <pw:package pw:id="${req.params.id}"/>
+          </momex:criteria>
+          </transaction>
   `;
 
       const response = await axios.post("https://jpp.pharmacywire.com/momex/NavCode/xmlconnect",body);
